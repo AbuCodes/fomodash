d0be4d90b041c78c5606c9c685f75b3a71226779
Abu Adel <abuadel251@gmail.com>
2019-07-19 20:02:28 -0600

    Updated code

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import AppLayout from "./AppLayout";
+import AppBar from "./AppBar";
+import { AppProvider } from "./AppProvider";
+import Settings from "../Settings";
+import Dashboard from "../Dashboard";
+import Content from "../Shared/Content";
 
-function App() {
-    return (
-        <div>
-            hello wods <h1>h1></h1>
-        </div>
-    );
+class App extends Component {
+    render() {
+        return (
+            <AppLayout>
+                <AppProvider>
+                    <AppBar />
+                    <Content>
+                        <Settings />
+                        <Dashboard />
+                    </Content>
+                </AppProvider>
+            </AppLayout>
+        );
+    }
 }
 
 export default App;
